6deb3cc80002a6f5051360500f763f9bc4f1f4c0
dev15031997 <devashishpal15@gmail.com>
2025-08-11 00:26:05 +0530

    frontend Ui setup

--- backend/app.js
+++ backend/app.js
@@ -2,14 +2,17 @@ require('dotenv').config();
 const express=require('express')
 const app=express();
 const cors=require('cors')
+require('./Db/conn')
 
 let port=process.env.PORT || 5000;
 
 app.use(cors({
-    origin:'http://localhost:5173',
+    origin:'http://localhost:3000',
     methods:'GET,POST,PUT,DELETE',
     credentials:true
 }))
+
+app.use(express.json())
 
 
 app.listen(port,()=>{
